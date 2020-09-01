802649933535fa674cebbfe7e10fe44109f69430
mathieudutour <mathieu@dutour.me>
2020-09-01 15:50:30 +0200

    unique results

--- _layouts/src/search.js
+++ _layouts/src/search.js
@@ -78,9 +78,18 @@ export const useFlexSearch = (query, searchOptions) => {
       ? titleIndex.search(query, searchOptions)
       : [];
 
+    const uniqIds = new Set();
+
     return rawPathResults
       .concat(rawTitleResults)
       .concat(rawBodyResults)
+      .filter((id) => {
+        if (uniqIds.has(id)) {
+          return false;
+        }
+        uniqIds.add(id);
+        return true;
+      })
       .map((id) => store[id]);
   }, [query, pathIndex, titleIndex, bodyIndex, store]);
 };
